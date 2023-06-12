8060291fb24ba61751d731918aacb8a68e58f5b1
Martin Jakobsson <martin@backtick.se>
2023-06-12 15:20:01 +0200

    OpenAI integration

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from "next/server";
+import { OpenAiClient } from "./OpenAI";
+import { env } from "process";
 
 export type PromptInput = { prompt: string };
 export type PromptResponse = { response: string };
 
+const openAiKey = env.OPENAI_KEY;
+if (!openAiKey?.length) throw new Error("No OpenAI key provided!");
+const openAi = new OpenAiClient(openAiKey);
+
 export async function POST(req: Request) {
   const { prompt }: PromptInput = await req.json();
 
-  const res: PromptResponse = { response: `You said "${prompt}".` };
+  const response = await openAi.prompt(prompt);
+
+  const res: PromptResponse = { response };
 
   return NextResponse.json(res);
 }
